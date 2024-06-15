8a83aa842695e4e54dac29ad17dd68e56f221448
root <root@localhost>
2024-06-15 12:43:19 +0200

    latest kernel

--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,9 @@
       ./hardware-configuration.nix
     ];
 
+  # Kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
