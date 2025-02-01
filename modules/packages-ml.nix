050e509bf045248ba46f65505aef7f5016f6bd31
root <root@localhost>
2025-02-01 14:19:07 +0100

    vulkan crashes unfortunately

--- modules/packages-ml.nix
+++ modules/packages-ml.nix
@@ -1,13 +1,15 @@
 # Packages - LLMs, etc.
 {
   config,
+  pkgs,
   unstablePkgs,
   ...
 }: {
   environment.systemPackages = with unstablePkgs; [
     (llama-cpp.override
       {vulkanSupport = true;})
+
     (whisper-cpp-vulkan.override
-      {vulkanSupport = true;})
+      {vulkanSupport = false;})
   ];
 }
