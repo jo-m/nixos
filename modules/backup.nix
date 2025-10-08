64e6c854992e2fc6d1bf133151f62e00374f7248
Jonathan Müller <jo-m@outlook.com>
2025-10-08 20:56:32 +0200

    give up

--- modules/backup.nix
+++ modules/backup.nix
@@ -7,9 +7,9 @@
   unstablePkgs,
   ...
 }: let
-  borgRepoKeyKeepassUrl = "borg-passphrase://backup";
-  printBorgRepoKey = pkgs.writeShellApplication {
-    name = "printBorgRepoKey";
+  borg-repo-key-keepass-url = "borg-passphrase://backup";
+  borg-print-repo-key = pkgs.writeShellApplication {
+    name = "borg-print-repo-key";
 
     runtimeInputs = with pkgs; [
       coreutils
@@ -25,13 +25,13 @@
 
       home="$(getent passwd ${config.custom.unprivilegedUser} | cut -d: -f6)"
 
-      echo 'url=${borgRepoKeyKeepassUrl}' \
+      echo 'url=${borg-repo-key-keepass-url}' \
         | git-credential-keepassxc --config "$home/.config/git-credential-keepassxc" get \
         | sed -n 's/^password=//p'
     '';
   };
-  userRsh = pkgs.writeShellApplication {
-    name = "userRsh";
+  borg-rsh = pkgs.writeShellApplication {
+    name = "borg-rsh";
 
     runtimeInputs = with pkgs; [
       coreutils
@@ -48,44 +48,10 @@
       sudo -u ${config.custom.unprivilegedUser} --preserve-env=SSH_AUTH_SOCK ssh "$@"
     '';
   };
-  archiveName = "test"; # TODO
-  postfix = "-${config.networking.hostName}-${archiveName}";
 in {
-  # Start and monitor manually:
-  #   sudo systemctl start borgbackup-job-test.service
-  #   journalctl -fu borgbackup-job-test.service
-  services.borgbackup.jobs.test = {
-    doInit = false;
-    appendFailedSuffix = true; # TODO: Clean those up.
-    preHook = ''
-      set -x
-
-      archiveName="$(date +%Y-%m-%d_%H-%M-%S)${postfix}"
-    '';
-    extraCreateArgs = "--verbose --stats";
-    paths = "/boot";
-    encryption.mode = "repokey";
-    encryption.passCommand = lib.getExe printBorgRepoKey;
-    # removableDevice=true; # TODO: Set
-    environment = {
-      BORG_REMOTE_PATH = "borg1";
-      BORG_RSH = lib.getExe userRsh;
-    };
-    # `rsync.net` needs a matching entry in `~/.ssh/config`.
-    repo = "rsync.net:backup.borg"; # TODO: backup.borg
-    compression = "zstd";
-    startAt = []; # TODO: "daily";
-
-    exclude = []; # TODO: fill in.
-
-    prune.keep = {
-      within = "1d"; # Keep all archives from the last day
-      daily = 7;
-      weekly = 8;
-      monthly = 12;
-      yearly = -1;
-    };
-    prune.prefix = null;
-    extraPruneArgs = "--glob-archives ${lib.escapeShellArg "*${postfix}"}";
-  };
+  # Export this for the backup scripts.
+  environment.systemPackages = [
+    borg-print-repo-key
+    borg-rsh
+  ];
 }
