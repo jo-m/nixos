37e0e08a5bd1959980e732eb29d2f4d4f31b2277
Jonathan Müller <jo-m@outlook.com>
2025-11-23 22:09:09 +0100

    add nix-ld and envfs

--- modules/packages-dev-tools.nix
+++ modules/packages-dev-tools.nix
@@ -5,6 +5,22 @@
   unstablePkgs,
   ...
 }: {
+  # https://fzakaria.com/2025/02/26/nix-pragmatism-nix-ld-and-envfs
+  programs = {
+    nix-ld = {
+      enable = true;
+      libraries = with pkgs; [
+        stdenv.cc.cc.lib
+        zlib
+      ];
+    };
+  };
+  services = {
+    envfs = {
+      enable = true;
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     aflplusplus
     android-tools
