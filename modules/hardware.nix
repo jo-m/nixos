13875bfcca7a8778d2cc97f5ada5e6363fadb9fa
Jonathan M <jo-m@outlook.com>
2026-02-08 22:32:43 +0100

    remove unused intel NPU/VPU firmware

--- modules/hardware.nix
+++ modules/hardware.nix
@@ -17,26 +17,6 @@
   };
   environment.sessionVariables = {LIBVA_DRIVER_NAME = "iHD";}; # Force intel-media-driver
 
-  # Intel NPU/VPU firmware
-  hardware.firmware = [
-    (
-      let
-        model = "37xx";
-        version = "v1.5.1";
-        fwVersion = "v0.0";
-
-        firmware = pkgs.fetchurl {
-          url = "https://github.com/intel/linux-npu-driver/raw/${version}/firmware/bin/vpu_${model}_${fwVersion}.bin";
-          hash = "sha256-KVO9EF/faYV1g2st59fIgEHqCYIgM+JhSSfzOlcA7SU=";
-        };
-      in
-        pkgs.runCommand "intel-vpu-firmware-${model}-${fwVersion}" {} ''
-          mkdir -p "$out/lib/firmware/intel/vpu"
-          cp '${firmware}' "$out/lib/firmware/intel/vpu/vpu_${model}_${fwVersion}.bin"
-        ''
-    )
-  ];
-
   # DDC
   hardware.i2c.enable = true;
   boot.extraModulePackages = [config.boot.kernelPackages.ddcci-driver];
