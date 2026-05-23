4a7b37a56b66dabac9e24c0640a60db1a3b28b8f
Jonathan M <jo-m@outlook.com>
2026-05-23 23:53:19 +0200

    Revert "disable i915 GuC"
    This reverts commit 622c451129598dbe464a817824231905a17b79f7.

--- modules/kernel-boot.nix
+++ modules/kernel-boot.nix
@@ -8,15 +8,6 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelParams = [
     "mitigations=off"
-
-    # Disable GuC, hoping to reduce crashes like this one:
-    #  i915 0000:00:02.0: [drm] *ERROR* GT0: GUC: TLB invalidation response timed out for seqno 3748100
-    #  i915 0000:00:02.0: [drm] *ERROR* GT0: GUC: TLB invalidation response timed out for seqno 3748101
-    #  i915 0000:00:02.0: [drm] GPU HANG: ecode 12:0:00000000
-    #  i915 0000:00:02.0: [drm] GPU error state saved to /sys/class/drm/card1/error
-    #  i915 0000:00:02.0: [drm] GT0: Resetting chip for stopped heartbeat on rcs0
-    # TODO: Eventually enable this again, especially if we still crash.
-    "i915.enable_guc=0"
   ];
 
   zramSwap = {
